f5b30bcc3ca1b937013f7acb824c9d267657c828
shubham pandey <spandeyji77@gmail.com>
2026-05-04 21:00:10 +0530

    update

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ openBtn.addEventListener("click", () => {
   const entry = document.getElementById("entry");
   const main = document.getElementById("main");
   const music = document.getElementById("bg-music");
+  const btn = document.getElementById("openBtn");
+  const particleContainer = btn.querySelector(".particles");
+  
+  btn.addEventListener("click", () => {
 
+  for (let i = 0; i < 40; i++) {   // 🔥 increase count
+    const dot = document.createElement("span");
+
+    dot.style.left = "50%";
+    dot.style.top = "50%";
+
+    const angle = Math.random() * 360;
+    const distance = Math.random() * 150;
+
+    dot.style.setProperty("--x", Math.cos(angle) * distance + "px");
+    dot.style.setProperty("--y", Math.sin(angle) * distance + "px");
+
+    particleContainer.appendChild(dot);
+
+    setTimeout(() => dot.remove(), 1000);
+  }
+
+});
   // DOOR OPEN
   left.style.transform = "translateX(-100%)";
   right.style.transform = "translateX(100%)";
