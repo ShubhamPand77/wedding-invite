307b019a5f3fa3a3387cdb78dd5facf46adb58d6
shubham pandey <spandeyji77@gmail.com>
2026-05-04 20:36:46 +0530

    update

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-document.getElementById("openBtn").addEventListener("click", function () {
+const openBtn = document.getElementById("openBtn");
 
+openBtn.addEventListener("click", () => {
   const left = document.querySelector(".left");
   const right = document.querySelector(".right");
   const entry = document.getElementById("entry");
   const main = document.getElementById("main");
   const music = document.getElementById("bg-music");
 
-  console.log("GATE OPEN");
-
-  // 🚪 Split animation
+  // DOOR OPEN
   left.style.transform = "translateX(-100%)";
   right.style.transform = "translateX(100%)";
 
-  // 🎵 music start
+  // MUSIC
   music.volume = 0;
   music.play();
 
@@ -26,7 +25,7 @@ document.getElementById("openBtn").addEventListener("click", function () {
     }
   }, 200);
 
-  // ✨ show main after gate opens
+  // SHOW MAIN
   setTimeout(() => {
     entry.style.display = "none";
     main.style.display = "flex";
@@ -36,35 +35,9 @@ document.getElementById("openBtn").addEventListener("click", function () {
     }, 100);
 
   }, 1500);
-
 });
-const container = document.querySelector(".particles");
 
-for (let i = 0; i < 30; i++) {
-  const dot = document.createElement("span");
-
-  dot.style.left = Math.random() * 100 + "%";
-  dot.style.animationDuration = (6 + Math.random() * 6) + "s";
-  dot.style.width = dot.style.height = (2 + Math.random() * 4) + "px";
-
-  container.appendChild(dot);
-}
-.burst {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
-  z-index: 9;
-}
-
-.burst span {
-  position: absolute;
-  width: 6px;
-  height: 6px;
-  background: #d4af37;
-  border-radius: 50%;
-  opacity: 1;
-}
+/* SCROLL ANIMATION */
 const sections = document.querySelectorAll(".section");
 
 window.addEventListener("scroll", () => {
